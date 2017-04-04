4c9b796fd5620b2c1ae57cfc0271b9ac334f8899
root <root@fenice.serverkreos.eu>
2017-04-05 01:04:29 +0200

    + add italian language plugin social

---
+++ e107_plugins/social/social/languages/Italian/Italian_admin.php
@@ -0,0 +1,52 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|     social plugin
+
++----------------------------------------------------------------------------+
+*/
+define("LAN_SOCIAL_ADMIN_SUMM", "Aggiungi Facebook, Twitter ed altri social media widgets in e107.");
+define("LAN_SOCIAL_ADMIN_00", "Applicazioni");
+define("LAN_SOCIAL_ADMIN_01", "Pagine");
+define("LAN_SOCIAL_ADMIN_02", "Iscriversi/Autenticarsi a mezzo Social");  //tab
+define("LAN_SOCIAL_ADMIN_03", "Iscrizione/Login");  //bar
+define("LAN_SOCIAL_ADMIN_04", "Provider");
+define("LAN_SOCIAL_ADMIN_05", "Key/ID");
+define("LAN_SOCIAL_ADMIN_06", "Segreta");
+define("LAN_SOCIAL_ADMIN_07", "Permetti agli utenti di iscriversi/autenticarsi a mezzo dei loro account social media. Se si abilita questa opzione sarà consentita l'iscrizione / login anche se il sistema di registrazione utente di base di cui sopra è disattivato.");
+define("LAN_SOCIAL_ADMIN_08", "Nota: Nella maggior parte dei casi sarà necessario ottenere un ID e una chiave segreta da uno dei fornitori. \nFare clic sui link blu a destra per configurare. \n \nPuoi testare la configurazione con il seguente URL:");
+define("LAN_SOCIAL_ADMIN_09", "Il tuo URL di ritorno é: ");
+define("LAN_SOCIAL_ADMIN_10", "Ottinei la chiave dal fornitore");
+define("LAN_SOCIAL_ADMIN_11", "Tuo");
+define("LAN_SOCIAL_ADMIN_12", "pagina");
+define("LAN_SOCIAL_ADMIN_13", "Utilizzato da alcuni temi per fornire un link al tuo ");
+define("LAN_SOCIAL_ADMIN_14", "Pulsanti di Condivisione");
+define("LAN_SOCIAL_ADMIN_15", "Commenti Facebook");
+define("LAN_SOCIAL_ADMIN_16", "Menu Facebook");
+define("LAN_SOCIAL_ADMIN_17", "Menu Twitter");
+define("LAN_SOCIAL_ADMIN_18", "Limite");
+define("LAN_SOCIAL_ADMIN_19", "Template");
+define("LAN_SOCIAL_ADMIN_20", "Azione");
+define("LAN_SOCIAL_ADMIN_21", "Testo durante il caricamento");
+define("LAN_SOCIAL_ADMIN_22", "Larghezza");
+define("LAN_SOCIAL_ADMIN_23", "Altezza");
+define("LAN_SOCIAL_ADMIN_24", "Modalità Display");
+define("LAN_SOCIAL_ADMIN_25", "Normale");
+define("LAN_SOCIAL_ADMIN_26", "Dropdown");
+define("LAN_SOCIAL_ADMIN_27", "Disabilitato");
+define("LAN_SOCIAL_ADMIN_28", "Escludendo il simbolo # .");
+define("LAN_SOCIAL_ADMIN_29", "Numero di commenti da visualizzare.");
+define("LAN_SOCIAL_ADMIN_30", "Caricamento...");
+define("LAN_SOCIAL_ADMIN_31", "Larghezza in px");
+define("LAN_SOCIAL_ADMIN_32", "Raccomandato");
+define("LAN_SOCIAL_ADMIN_33", "Altezza in px");
+define("LAN_SOCIAL_ADMIN_34", "Numero di tweets da visualizzare.");
+define("LAN_SOCIAL_ADMIN_35", "Chiaro");
+define("LAN_SOCIAL_ADMIN_36", "Scuro");
+define("LAN_SOCIAL_ADMIN_37", "Open Graph Meta");
+define("LAN_SOCIAL_ADMIN_38", "Scopo");
+define("LAN_SOCIAL_ADMIN_39", "Providers");
+define("LAN_SOCIAL_ADMIN_40", "Aggiorna nome utente visualizzato");
+define("LAN_SOCIAL_ADMIN_41", "Aggiorna Avatar utente");
+define("LAN_SOCIAL_ADMIN_42", "Immagine Personalizzata");
---
+++ e107_plugins/social/social/languages/Italian/Italian_front.php
@@ -0,0 +1,27 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|     social plugin
+
++----------------------------------------------------------------------------+
+*/
+//LAN_SOCIAL_000 to LAN_SOCIAL_099 // Common parts of the frontend
+define("LAN_SOCIAL_000", "Condividi Su [x]");
+define("LAN_SOCIAL_001", "Like su [x]");
+define("LAN_SOCIAL_002", "Email a qualcuno");
+define("LAN_SOCIAL_003", "+1 su Google");
+define("LAN_SOCIAL_004", "Aggiungi a [x]");
+define("LAN_SOCIAL_005", "Guarda questo link:");
+
+
+//LAN_SOCIAL_100 to LAN_SOCIAL_199 // Facebook specific
+define("LAN_SOCIAL_100",  "Impossibile visualizzare feed. Facebook App ID non è stato definito nelle preferenze.");
+
+
+//LAN_SOCIAL_200 to LAN_SOCIAL_299 // Twitter specific
+define("LAN_SOCIAL_200",  "Disabilita la visualizzazione dei feed. L'URL di Twitter non è stato definito nelle preferenze.");
+define("LAN_SOCIAL_201",  "Tweets da");
+define("LAN_SOCIAL_202",  "Post su Twitter");
+define("LAN_SOCIAL_203",  "Scrivi il tuo tweet qui.");
+
